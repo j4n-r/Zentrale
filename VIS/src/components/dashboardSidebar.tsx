da6bc1eb56212ba899d76cd69da4eeb0cc065d0f
j4n-r <jan.ruegge@gmail.com>
2025-08-15 19:01:33 +0200

    add rose pine theme, refactor sidebar

--- VIS/src/components/dashboardSidebar.tsx
+++ VIS/src/components/dashboardSidebar.tsx
@@ -1,5 +1,14 @@
-import { Calendar, Home, Network, Search, Settings } from "lucide-react";
+"use client";
 
+import {
+  Calendar,
+  Home,
+  Network,
+  Search,
+  Settings,
+  Moon,
+  Sun,
+} from "lucide-react";
 import {
   Sidebar,
   SidebarContent,
@@ -10,34 +19,14 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-
-const items = [
-  {
-    title: "Home",
-    url: "#",
-    icon: Home,
-  },
-  {
-    title: "Network",
-    url: "network",
-    icon: Network,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-];
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "@/components/theme-provider";
 
 export function DashboardSidebar() {
   return (
@@ -47,16 +36,75 @@ export function DashboardSidebar() {
           <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href="#">
+                    <Home />
+                    <span>Home</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href="network">
+                    <Network />
+                    <span>Network</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href="#">
+                    <Calendar />
+                    <span>Calendar</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href="#">
+                    <Search />
+                    <span>Search</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href="#">
+                    <Settings />
+                    <span>Settings</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        <SidebarGroup className="mt-auto">
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <SidebarMenuButton>
+                      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                      <span>Toggle theme</span>
+                    </SidebarMenuButton>
+                  </DropdownMenuTrigger>
+                  {/* <DropdownMenuContent align="end"> */}
+                  {/*   <DropdownMenuItem onClick={() => setTheme("light")}> */}
+                  {/*     Light */}
+                  {/*   </DropdownMenuItem> */}
+                  {/*   <DropdownMenuItem onClick={() => setTheme("dark")}> */}
+                  {/*     Dark */}
+                  {/*   </DropdownMenuItem> */}
+                  {/*   <DropdownMenuItem onClick={() => setTheme("system")}> */}
+                  {/*     System */}
+                  {/*   </DropdownMenuItem> */}
+                  {/* </DropdownMenuContent> */}
+                </DropdownMenu>
+              </SidebarMenuItem>
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
